1d973349579e1e36ddf3ed4f0927f8e1b5ec0d13
Rhett Harrison <reharri7@asu.edu>
2020-09-30 16:20:22 -0700

    Updates

---
+++ precache-manifest.cb11f34463ea0bc5b9100f30f89ed992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c90952889ddf371dc173335fceea17",
+    "url": "/e-commerce-template/index.html"
+  },
+  {
+    "revision": "f459d85c38a013a3372a",
+    "url": "/e-commerce-template/static/css/main.59c5921a.chunk.css"
+  },
+  {
+    "revision": "ad726fd5e75f8567f2da",
+    "url": "/e-commerce-template/static/js/2.3514e4e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-commerce-template/static/js/2.3514e4e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f459d85c38a013a3372a",
+    "url": "/e-commerce-template/static/js/main.852e8b67.chunk.js"
+  },
+  {
+    "revision": "4d7d37c68c5fdbcf9c27",
+    "url": "/e-commerce-template/static/js/runtime-main.be6004c4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-template/static/media/crown.ed28accb.svg"
+  }
+]);
